7e93377ca0cef51bdadae6bf43905dae23f47b8d
Åsmund Våge Fannemel <34712686+asmfstatoil@users.noreply.github.com>
2026-05-26 20:49:12 +0200

    s

--- src/msal_bearer/authenticator.py
+++ src/msal_bearer/authenticator.py
@@ -50,7 +50,6 @@ class Authenticator:
             self.authority = f"https://login.microsoftonline.com/{tenant_id}"
         else:
             self.authority = None
-
         self.redirect_uri = redirect_uri
         self.token = ""
         self.user_name = user_name
